55ce2acd86f3712b6ce99f08601d46e47e0d803b
Aashish Gupta <aashishg008@gmail.com>
2026-07-05 19:06:41 +0530

    Delete At_Percentage_Calculator directory

--- At_Percentage_Calculator/at_percentage_calculator.py
+++
@@ -1,8 +0,0 @@
-sequence=input("Enter Sequence:").upper()
-count=0
-for at in sequence:
-    if  at=='A' or at=='T':
-        count=count+1
-lenght=len(sequence)
-AT=(count/lenght)*100
-print("AT% Of The Given Sequence Is:",AT)
